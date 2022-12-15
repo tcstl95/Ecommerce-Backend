211631bc5f1891b3f0267f9317bd5f40c9d3e4dc
Tom Carlson <tomcarlson171@gmail.com>
2022-12-14 23:31:47 -0600

    commit

--- api/tag-routes.js
+++ api/tag-routes.js
@@ -22,21 +22,69 @@ router.get('/', async(req, res) => {
     }
   });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne({
+    where:{
+      id:req.params.id
+    },
+      attributes:['id','tag_name'],
+      include: [
+      {
+        model:Product,
+        attributes:['id','product_name','price','stock','category_id']
+      },
+      ]
+      });
+      res.json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/',async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+    tag_name: req.body.tag_name
+    })
+      res.json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body,{
+     where: {
+      id: req.params.id
+     }
+     
+    });
+    res.json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy(req.body,{
+     where: {
+      id: req.params.id
+     }
+     
+    });
+    res.json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const routes = require('./routes');
 const sequelize= require('./config/connection');
+const mysql= require('mysql2');
 // import sequelize connection
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL Username
+    user: 'root',
+    // TODO: Add MySQL Password
+    password: 'Stlblues#1',
+    database: 'ecommerce_db'
+  },
+  console.log(`Connected to the ecommerce_db database.`)
+);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,10 +20,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, host, () => console.log(`Now listening ${PORT}.`));
 });
 
