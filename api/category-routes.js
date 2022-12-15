b074aeac190ea59b288f4dba9a430331a2d8272a
Tom Carlson <tomcarlson171@gmail.com>
2022-12-14 19:33:35 -0600

    initial commit

---
+++ api/category-routes.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+Category.findAll({
+  attributes: ['id', 'category_name'],
+  include:[{
+    model: Product,
+    attributes: ['id', 'product','price', 'stock','category_name' ]
+  }
+
+  ]
+})
+
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  Category.findOne({
+    where:{
+      id:req.params.id
+
+    },
+    attributes: ['id', 'category_name'],
+    })
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
