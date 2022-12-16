dd35c3e668179fde6d155c8918cb6bbeb163f930
Tom Carlson <tomcarlson171@gmail.com>
2022-12-15 19:17:40 -0600

    made adjustments to file strucutre

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sequelize= require('./config/connection');
 const mysql= require('mysql2');
+const routes = require('./api');
 // import sequelize connection
 const db = mysql.createConnection(
   {
@@ -19,11 +20,12 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, host, () => console.log(`Now listening ${PORT}.`));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:3001.`));
 });
 
