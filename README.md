0e37769c2260dd1485def56a92e1fc4b6720bcec
Tom Carlson <tomcarlson171@gmail.com>
2022-12-15 21:54:43 -0600

    Added notes to ReadMe and made adjustments to file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Ecommerce-Backend
+
+# Description
+
+Even though this application has not been completed yet. The purpose of this application is to help create a back end for the data for an Ecommerce company.
+
+This can come in handy, especially since ecommerce plays a major role in our current society and generates billions of dollars.
+
--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const sequelize= require('./config/connection');
-const mysql= require('mysql2');
 const routes = require('./api');
 // import sequelize connection
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL Username
-    user: 'root',
-    // TODO: Add MySQL Password
-    password: 'Stlblues#1',
-    database: 'ecommerce_db'
-  },
-  console.log(`Connected to the ecommerce_db database.`)
-);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,6 +15,6 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening on http://localhost:3001.`));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}.`));
 });
 
